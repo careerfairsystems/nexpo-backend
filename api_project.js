8590c5dc299653dffe61254ba879d71ffe2bf1b5
emarforio <emarforio@gmail.com>
2021-08-17 18:09:48 +0000

    Deploying to gh-pages from @ careerfairsystems/nexpo-backend@60b85bb2a32e8c3a909aabc36437458044871318 🚀

---
+++ api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Nexpo Backend",
+  "version": "1.0.0",
+  "description": "An internal system for ARKAD",
+  "title": "Nexpo Backend - Documentation",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-08-17T18:09:48.345Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
